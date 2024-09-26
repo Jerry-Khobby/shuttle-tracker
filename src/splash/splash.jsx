b7ff6cb8843ff858e1d5622217e21975ffb12421
Jerry-Khobby <jerrymardeburg@gmail.com>
2024-09-26 04:45:28 -1200

    I changed the splash screen

---
+++ src/splash/splash.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View,Text } from 'react-native';
+
+
+
+const SplashScreen = () => {
+  return ( 
+    <View className="flex items-center justify-center h-full w-full flex-1 mt-20">
+      <Text>Hello</Text> 
+
+    </View>
+   );
+}
+ 
+export default SplashScreen;
